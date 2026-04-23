f7c6a96d3a6f60d9683acd200b7c718a92816078
BerkeeA <276627543+BerkeeA@users.noreply.github.com>
2026-04-23 19:56:16 +0300

    Add AIAssistant class

---
+++ logic/AIAssistant.java
@@ -0,0 +1,32 @@
+package logic;
+
+import models.User;
+import models.Device;
+
+public class AIAssistant{
+    private int assistantID;
+    private String name;
+
+    public AIAssistant(int assistantID,String name){
+        this.assistantID=assistantID;
+        this.name=name;
+    }
+
+    public boolean authenticateVoice(User u){
+        System.out.println("Authenticating voice for: " + u.getName());
+        return true; 
+    }
+
+    public void processVoiceCommand(String cmd){
+        System.out.println("Processing command: " + cmd);
+    }
+
+    public void executeAction(Device d){
+        System.out.println("Executing action on device: " + d.getName());
+        d.turnOn();
+    }
+
+    public void updateDatabase(Device d){
+        System.out.println("Database updated for: " + d.getName());
+    }
+}

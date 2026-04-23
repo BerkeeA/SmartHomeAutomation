5d4d06533d7b8c5a2cbd50a94fabb5a246556548
BerkeeA <276627543+BerkeeA@users.noreply.github.com>
2026-04-23 16:01:15 +0300

    Add Thermostat class

---
+++ models/Thermostat.java
@@ -0,0 +1,19 @@
+package models;
+
+public class Thermostat extends Device{
+    private int temperature;
+
+    public Thermostat(int id, String name, String roomName){
+        super(id,name,roomName);
+        this.temperature=22;
+    }
+
+    public void setTemperature(int value){
+        this.temperature=value;
+        System.out.println(name + " temperature set to " + value + " degrees.");
+    }
+
+    public int getTemperature(){
+        return temperature;
+    }
+}

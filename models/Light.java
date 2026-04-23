035dbe8e5de9ba5f64ac1e02a91cdb7988721736
BerkeeA <276627543+BerkeeA@users.noreply.github.com>
2026-04-23 15:58:35 +0300

    Add Light class

---
+++ models/Light.java
@@ -0,0 +1,25 @@
+package models;
+
+public class Light extends Device{
+    private int brightness;
+
+    public Light(int id,String name,String roomName){
+        super(id,name,roomName);
+        this.brightness=100;
+    }
+
+    @Override
+    public void turnOn(){
+        this.status = "ON";
+        System.out.println(name + " in " + roomName + " is now ON.");
+    }
+
+    public void adjustBrightness(int value){
+        this.brightness=value;
+        System.out.println(name + " brightness adjusted to " + value + ".");
+    }
+
+    public int getBrightness(){
+        return brightness;
+    }
+}

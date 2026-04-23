1a9292893b2e1f6597b17a92f6eba31365311daf
BerkeeA <276627543+BerkeeA@users.noreply.github.com>
2026-04-23 20:01:03 +0300

    Add VoiceInteractionModule class

---
+++ logic/VoiceInteractionModule.java
@@ -0,0 +1,22 @@
+package logic;
+
+public class VoiceInteractionModule{
+    private String apiID;
+    private String roomName;
+    private boolean isActive;
+
+    public VoiceInteractionModule(String apiID, String roomName){
+        this.apiID=apiID;
+        this.roomName=roomName;
+        this.isActive=true;
+    }
+
+    public void receiveVoiceInput(String text){
+        System.out.println("Voice input received: " + text);
+        sendToAssistant(text);
+    }
+
+    public void sendToAssistant(String text){
+        System.out.println("Sending command to AI via API: " + apiID);
+    }
+}
